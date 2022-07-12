e09b1420103a311dc3e00105a81964ceb66aad64
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-07-12 11:42:21 -0400

    Create FizzBuzz.java

---
+++ FizzBuzz.java
@@ -0,0 +1,18 @@
+public class FizzBuzz {
+        public static void main(String[] args) {
+            int x = 15;
+            for(int i = 1; i<x + 1;++i){
+                String resp = "";
+                if(i % 3 == 0){
+                    resp = resp + "Fizz";
+                }
+                if(i % 5 == 0){
+                    resp = resp + "Buzz";
+                }
+                if(i % 3 != 0 && i % 5 != 0){
+                    resp = String.valueOf(i);
+                }
+                System.out.println(resp);
+            }
+        }
+}
